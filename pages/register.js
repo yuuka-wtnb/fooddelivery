11bc9d4b935076e795af1eec55fd92830a09055d
yuuka-wtnb <yuuka.wtnb.011@gmail.com>
2023-08-01 04:32:10 +0900

    アカウント登録用のページを作成

---
+++ pages/register.js
@@ -0,0 +1,53 @@
+import { Button, Col, Container, Form, FormGroup, Input, Label, Row } from "reactstrap";
+
+const register = () => {
+    return (
+       <Container>
+        <Row>
+            <Col>
+            <div className="paper">
+                <div className="header">
+                    <h2>ユーザー登録</h2>
+                </div>
+            </div>
+            <section className="wrapper">
+                <Form>
+                    <fieldset>
+                        <FormGroup>
+                            <Label>ユーザー名：</Label>
+                            <Input type="text" name="username" style={{height:50, fontSize:"1.2rem"}}></Input>
+                        </FormGroup>
+                    </fieldset>
+                </Form>
+
+                <Form>
+                    <fieldset>
+                        <FormGroup>
+                            <Label>メールアドレス：</Label>
+                            <Input type="email" name="email" style={{height:50, fontSize:"1.2rem"}}></Input>
+                        </FormGroup>
+                    </fieldset>
+                </Form>
+
+                <Form>
+                    <fieldset>
+                        <FormGroup>
+                            <Label>パスワード：</Label>
+                            <Input type="password" name="password" style={{height:50, fontSize:"1.2rem"}}></Input>
+                        </FormGroup>
+                    </fieldset>
+                </Form>
+                <span>
+                    <a href=""><small>パスワードをお忘れですか？</small></a>
+                </span>
+                <Button style={{float:"right", width: 120}} color="primary">
+                    登録
+                </Button>
+            </section>
+            </Col>
+        </Row>
+       </Container>
+    );
+}
+
+export default register;
