a9555d9c4d75c23580e13888e1021b6c12f79ff7
yuuka-wtnb <yuuka.wtnb.011@gmail.com>
2023-08-01 03:09:10 +0900

    料理データをページに出力して表示

--- pages/restaurants.js
+++ pages/restaurants.js
@@ -2,99 +2,91 @@ import { Card, CardBody, CardImg, CardTitle, Col, Row } from "reactstrap";
 import Link from "next/link";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 //variablesがrestaurant(id: $id)の$idに設定されるようになる
 const GET_RESTAURANT_DISHES = gql`
-  {
-    query($id: ID!){
-        restaurant(id: $id){
-            id
-            name
-            dishes{
-                id
-                name
-                description
-                price
-                image{
-                    url
-                }
-            }
+  query ($id: ID!) {
+    restaurant(id: $id) {
+      id
+      name
+      dishes {
+        id
+        name
+        description
+        price
+        image {
+          url
         }
+      }
     }
   }
 `;
 
-
 const Restaurants = (props) => {
-    const router = useRouter();
-  const { loading, error, data } = useQuery(GET_RESTAURANT_DISHES,{
+  const router = useRouter();
+  const { loading, error, data } = useQuery(GET_RESTAURANT_DISHES, {
     // ここのqueryはhref={`/restaurants?id=${res.id}`}のres.idをとってきてる
-    variables:{id:router.query.id},
+    variables: { id: router.query.id },
   });
 
   if (loading) return <h2>ロード中・・・</h2>;
-  if (error) return "レストランの読み込みに失敗しました"
+  if (error) return "レストランの読み込みに失敗しました";
 
-    if (data) {
+  if (data) {
+    const { restaurant } = data;
 
-      const searchQuery = data.restaurants.filter((restaurant) =>
-        restaurant.name.toLowerCase().includes(props.search)
-      );
-      return (
-        //xs="6" sm="4"はレスポンシブの設定
-        <Row>
-          {/* restaurantの頭文字resにして1つ1つ取り出す */}
-          {searchQuery.map((res) => (
-            <Col xs="6" sm="4" key={res.id}>
-              <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
-                {/* この${process.env.NEXT_PUBLIC_API_URL}は、.env.developmentから取ってきたもので
-                .NEXT_PUBLIC_API_URLっていうのを設定した
-                .envではじまるものは環境変数として使える。そしてlocalhost 1337番を↓で出力*/}
-                <CardImg
-                  src={`${process.env.NEXT_PUBLIC_API_URL}${res.image[0].url}`}
-                  top={true}
-                  style={{ height: 250 }}
-                >
-                  {/* topプロパティに trueが指定されているため、カードの上部に画像が表示されます。*/}
-                </CardImg>
-                <CardBody>
-                  <CardTitle>{res.name}</CardTitle>
-                  <CardTitle>{res.description}</CardTitle>
-                </CardBody>
-                <div className="card-footer"></div>
-                <Link
-                  as={`/restaurants/${res.id}`}
-                  href={`/restaurants?id=${res.id}`}
-                >
-                  <a className="btn btn-primary">もっとみる</a>
-                </Link>
-              </Card>
-            </Col>
-          ))}
+    return (
+      //xs="6" sm="4"はレスポンシブの設定
+      <Row>
+        {/* restaurantの中のdishesを1つ1つ取り出す */}
+        {restaurant.dishes.map((res) => (
+          <Col xs="6" sm="4" key={res.id}>
+            <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
+              <CardImg
+                src={`${process.env.NEXT_PUBLIC_API_URL}${res.image.url}`}
+                top={true}
+                style={{ height: 250 }}
+              >
+                {/* topプロパティに trueが指定されているため、カードの上部に画像が表示されます。*/}
+              </CardImg>
+              <CardBody>
+                <CardTitle>{res.name}</CardTitle>
+                <CardTitle>{res.description}</CardTitle>
+              </CardBody>
+              <div className="card-footer"></div>
+              <Link
+                as={`/restaurants/${res.id}`}
+                href={`/restaurants?id=${res.id}`}
+              >
+                <a className="btn btn-primary">もっとみる</a>
+              </Link>
+            </Card>
+          </Col>
+        ))}
 
-          <style jsx>
-            {`
-              a {
-                color: white;
-              }
-              a:link {
-                text-decoration: none;
-                color: white;
-              }
-              a:hover {
-                color: white;
-              }
-              .card-columns {
-                column-count: 3;
-              }
-            `}
-          </style>
-        </Row>
-      );
-    } else {
-      return <h1>レストランが見つかりませんでした</h1>;
-    }
+        <style jsx>
+          {`
+            a {
+              color: white;
+            }
+            a:link {
+              text-decoration: none;
+              color: white;
+            }
+            a:hover {
+              color: white;
+            }
+            .card-columns {
+              column-count: 3;
+            }
+          `}
+        </style>
+      </Row>
+    );
+  } else {
+    return <h1>レストランが見つかりませんでした</h1>;
+  }
 };
 
 export default Restaurants;
