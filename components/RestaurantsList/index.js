1fedeb4bb6d0eb44877e24d8f3cc7b78063b2519
yuuka-wtnb <yuuka.wtnb.011@gmail.com>
2023-08-01 02:37:09 +0900

    検索バーにおけるフィルタリング機能を実装

--- components/RestaurantsList/index.js
+++ components/RestaurantsList/index.js
@@ -21,15 +21,25 @@ const RestaurantList = (props) => {
   const { loading, error, data } = useQuery(query);
   if (loading) return <h2>ロード中・・・</h2>;
   if (data) {
+    //data.restaurantsの中にレストランの名前が入ってる
+    //restaurantを自分で名前を設定して1つずつチェック
+
+    //includes(props.search)これは、page/indexで<RestaurantList search={query}/>って
+    //設定してるからsearchになってる
+    //props.searchはsearch={query}をみてて、このqueryは設定したsetQuery(e.target.value.toLocaleLowerCase())
+    //検索で打ち込んだ文字列のこと
+
+    const searchQuery = data.restaurants.filter((restaurant) =>
+      restaurant.name.toLowerCase().includes(props.search)
+    );
     return (
       //xs="6" sm="4"はレスポンシブの設定
       <Row>
         {/* restaurantの頭文字resにして1つ1つ取り出す */}
-        {data.restaurants.map((res) => (
+        {searchQuery.map((res) => (
           <Col xs="6" sm="4" key={res.id}>
             <Card style={{ margin: "0 0.5rem 20px 0.5rem" }}>
-
-                {/* この${process.env.NEXT_PUBLIC_API_URL}は、.env.developmentから取ってきたもので
+              {/* この${process.env.NEXT_PUBLIC_API_URL}は、.env.developmentから取ってきたもので
                 .NEXT_PUBLIC_API_URLっていうのを設定した
                 .envではじまるものは環境変数として使える。そしてlocalhost 1337番を↓で出力*/}
               <CardImg
@@ -44,7 +54,10 @@ const RestaurantList = (props) => {
                 <CardTitle>{res.description}</CardTitle>
               </CardBody>
               <div className="card-footer"></div>
-              <Link href={`/restaurants/${res.id}`} as={`/restaurants?id=${res.id}`}>
+              <Link
+                href={`/restaurants/${res.id}`}
+                as={`/restaurants?id=${res.id}`}
+              >
                 <a className="btn btn-primary">もっとみる</a>
               </Link>
             </Card>
