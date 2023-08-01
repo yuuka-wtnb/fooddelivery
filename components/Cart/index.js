34d23ba4c9f3aa67ca06bc4cfe68c7be528686fe
yuuka-wtnb <yuuka.wtnb.011@gmail.com>
2023-08-01 18:44:31 +0900

    ショッピングカートをCSSスタイリング

--- components/Cart/index.js
+++ components/Cart/index.js
@@ -4,43 +4,93 @@ import Link from "next/link";
 const Cart = () => {
   return (
     <div>
-      <Card>
-        <CardTitle>注文一覧</CardTitle>
+      <Card style={{ padding: "10px 5px" }}>
+        <CardTitle
+          style={{
+            margin: 10,
+            textAlign: "center",
+            fontWeight: 600,
+            fontSize: 25,
+          }}
+        >
+          注文一覧
+        </CardTitle>
         {/* 横棒をひくタグ */}
         <hr />
-        <CardBody>
-          <div>
+        <CardBody style={{ padding: 10 }}>
+          <div style={{ marginBottom: 6 }}>
             <small>料理：</small>
           </div>
           <div>
-            <div className="items-one">
+            <div className="items-one" style={{ marginBottom: 15 }}>
               <div>
                 <span id="item-price">&nbsp; 200円</span>
                 <span id="item-name">&nbsp; サラダ</span>
               </div>
               <div>
-                <Button>+</Button>
-                <Button>-</Button>
+                <Button
+                  style={{
+                    height: 25,
+                    padding: 0,
+                    width: 15,
+                    marginRight: 5,
+                    marginLeft: 10,
+                  }}
+                  color="link"
+                >
+                  +
+                </Button>
+                <Button
+                  style={{
+                    height: 25,
+                    padding: 0,
+                    width: 15,
+                    marginRight: 5,
+                    marginLeft: 10,
+                  }}
+                  color="link"
+                >
+                  -
+                </Button>
                 {/*商品を注文した数 */}
-                <span id="item-quantity">1つ</span>
+                <span id="item-quantity" style={{ marginLeft: 5 }}>
+                  1つ
+                </span>
               </div>
             </div>
             <div>
-                <Badge>
-                    <h5>合計:</h5>
-                    <h3>1200円</h3>
-                </Badge>
-                <div>
-                    <Link href="/checkout">
-                        <Button>
-                            <a>注文する</a>
-                        </Button>
-                    </Link>
-                </div>
+              <Badge style={{ width: 200, padding: 10 }} color="light">
+                <h5 style={{ fontWeight: 100, color: "gray" }}>合計:</h5>
+                <h3>1200円</h3>
+              </Badge>
+              <div>
+                <Link href="/checkout">
+                  <Button style={{ width: "100%" }} color="primary">
+                    <a>注文する</a>
+                  </Button>
+                </Link>
+              </div>
             </div>
           </div>
         </CardBody>
       </Card>
+      <style jsx>
+        {`
+          #item-price {
+            font-size: 1.3em;
+            color: rgba(97, 97, 97, 1);
+          }
+          #item-quantity {
+            font-size: 0.95em;
+            padding-bottom: 4px;
+            color: rgba(158, 158, 158, 1);
+          }
+          #item-name {
+            font-size: 1.3em;
+            color: rgba(97, 97, 97, 1);
+          }
+        `}
+      </style>
     </div>
   );
 };
