1d5579f509a74f51db6edb0cfd52ac49c540fe75
yuuka-wtnb <yuuka.wtnb.011@gmail.com>
2023-08-02 01:50:32 +0900

    チェックアウトページをCSSスタイリング

--- pages/checkout.js
+++ pages/checkout.js
@@ -5,11 +5,11 @@ import CheckOutForm from "../components/Checkout/CheckOutForm";
 const checkout = () => {
     return (
         <Row>
-            <Col>
-            <h1>チェックアウト</h1>
+            <Col style={{paddingRight:0}} sm={{size:3, order:1, offset:2}}>
+            <h1 style={{margin:20, fontSize:20, textAlign:"center"}}>チェックアウト</h1>
             <Cart />
             </Col>
-            <Col>
+            <Col style={{paddingLeft:5}} sm={{size:6, order:2}}>
             <CheckOutForm />
             </Col>
         </Row>
